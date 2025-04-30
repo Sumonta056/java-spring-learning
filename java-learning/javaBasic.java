bba77f25deca8f16295638fa658935d64c1f923d
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2025-04-30 17:15:08 +0600

    setup: java spring boot

---
+++ java-learning/javaBasic.java
@@ -0,0 +1,7 @@
+public class javaBasic {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Print a message to the console
+        int number = 5; // Declare an integer variable
+        System.out.println("The number is: " + number); // Print the variable
+    }
+}
---
+++ java-spring/src/main/java/com/javaspring/java_spring/Hello.java
@@ -0,0 +1,12 @@
+package com.javaspring.java_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
